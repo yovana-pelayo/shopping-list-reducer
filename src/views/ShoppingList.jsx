622c5e0f4ccc04ed052ce3aa26dc9bf890c6d32a
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-05-16 02:03:29 -0700

    fixed syntax error and new note is displaying below the input. yay!

--- src/views/ShoppingList.jsx
+++ src/views/ShoppingList.jsx
@@ -3,23 +3,23 @@ import { useReducer, useState } from 'react';
 const initialNotes = [{ id: 0, text: 'Get cold brew please!', done: false }];
 
 // an array of info
-const noteReducer = (state, action) => {};
+const noteReducer = (state, action) => {
+  console.log('state', state);
+  console.log('action', action);
+};
 // function that updates our state {newNote}
 
 export default function ShoppingList() {
   const [notes, dispatch] = useReducer(noteReducer, initialNotes);
 
   const [newNote, setNewNote] = useState('');
-  //   const [notes, setNotes] = useState('');
-  //   //   useEffect(() => {
-  //   //    const data = await getNotes();
-
-  //   //     }
-  //   //   );
 
   const handleAddNote = (e) => {
     e.preventDefault();
+    dispatch({ type: 'add_note', payload: { text: newNote } });
   };
+
+  // when we call this handle function we are adding state by the type/action
   return (
     <div>
       <h2>Shopping List</h2>
@@ -30,12 +30,12 @@ export default function ShoppingList() {
           value={newNote}
           onChange={(e) => setNewNote(e.target.value)}
         />
-        <button className="save-button">add</button>
+        <button className="add-button">add</button>
       </form>
       <ul>
-        {notes.map((note) => {
-          <li key={note.id}>{note.text}</li>;
-        })}
+        {notes.map((note) => (
+          <li key={note.id}>{note.text}</li>
+        ))}
       </ul>
     </div>
   );
